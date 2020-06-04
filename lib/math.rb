5c4aaac707bbc352e268d052e24260549321f555
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-04 16:42:44 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  puts sum
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  puts sum
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  puts sum
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  puts sum
+  sum
 end
 
 def square_root(num)
-
+  sum = Math.sqrt(num)
+  puts sum
+  sum
 end
