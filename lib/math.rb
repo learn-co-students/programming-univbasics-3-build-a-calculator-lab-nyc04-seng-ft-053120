de625d6751a4ba23557f6098026d75f002a2ebd6
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-09 02:58:30 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+puts = num1 +num2
 end
+puts addition(5, 4)
 
 def subtraction(num1, num2)
-
+ puts = num1 -num2
 end
+subtraction(10, 5)
 
 def division(num1, num2)
-
+  puts = num1 /num2
 end
+ division(50, 2)
 
 def multiplication(num1, num2)
-
+  puts = num1 *num2
 end
+ multiplication(34, 5)
 
 def modulo(num1, num2)
 
+
+  puts = num1 %num2
 end
+ modulo(34, 5)
 
 def square_root(num)
-
+puts = Math.sqrt(81)
 end
+square_root(9)
